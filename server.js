349be8b5713e400be7af9c9d155efa4d5cb090f0
megan nickel <megansyd@live.unc.edu>
2021-11-28 21:07:53 -0500

    adding "run"

--- server.js
+++ server.js
@@ -28,7 +28,8 @@ app.get("/app/", (req, res, next) => {
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
 app.post("/app/new", (req,res) => {
 	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)");
-	res.status(200).json({"message":"1 record created: ID 3 (201)"});
+	const info = stmt.run("user = req.body.user", "pass = req.body.pass");
+	res.status(200).json({"message":"1 record created: ID req.params.id (201)"});
 });
 
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
@@ -46,14 +47,14 @@ app.get("/app/user/:id", (req, res) => {
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {
 	const stmt = db.prepare('UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?');
-	const info = stmt.run("user=newtest&pass=supersecurepassword");
+	const info = stmt.run("user = req.body.user", "pass = req.body.pass");
 	res.status(200).json(console.log(info.changes));
 });
 
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 app.delete("/app/delete/user/:id", (req, res) => {
-	const stmt = db.prepare('DELETE FROM userinfo WHERE id = 2');
-	res.status(200).json({"message":"1 record deleted: ID 2 (200)"});
+	const stmt = db.prepare('DELETE FROM userinfo WHERE id = ?');
+	res.status(200).json({"message":"1 record deleted: ID req.params.id (200)"});
 });
 
 // Default response for any other request
