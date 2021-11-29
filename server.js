1167b3038b6588cac62f7401f97849ab94fd8395
megan nickel <megansyd@live.unc.edu>
2021-11-28 23:35:51 -0500

    surely i found the issue

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get("/app/users", (req, res) => {
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
 app.get("/app/user/:id", (req, res) => {
-	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = 2").get();
+	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = " + req.params.id).get();
 	res.status(200).json(stmt);
 });
 
