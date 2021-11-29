cd3ba2fefd1ff91f3af5390dab6a836e4cb2b038
megan nickel <megansyd@live.unc.edu>
2021-11-28 22:10:35 -0500

    201 to 200

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get("/app/", (req, res, next) => {
 app.post("/app/new/", (req,res) => {
 	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)");
 	const info = stmt.run(req.body.user, md5(req.body.pass));
-	res.status(200).json({"message": info.changes + " record created: ID " + info.lastInsertRowid + " (201)"});
+	res.status(200).json({"message": info.changes + " record created: ID " + info.lastInsertRowid + " (200)"});
 });
 
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
